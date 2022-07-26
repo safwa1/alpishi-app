ce6801d980e6aa8c2efb16466f82df89db79338d
Safwan Abdulaghani <sfwanabdualghni@gmail.com>
2022-07-26 03:07:00 +0300

    fix bug 500

--- resources/views/livewire/website/index.blade.php
+++ resources/views/livewire/website/index.blade.php
@@ -1,14 +1,10 @@
 <div>
-    {{-- slider --}}
+    <!-- slider -->
     <livewire:slider />
-
     <!-- latest cars -->
     <x-commercial.latest :commercials="$latestCommercials" />
-
     <!-- purchase methods -->
     <x-purchase-methods.show />
-
     <!-- inquiries, ask-us -->
     <x-inquiries.show :phone-number="$phoneNumber"/>
-
 </div>
